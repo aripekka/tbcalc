3639ee9edbd07c8bc791e02fe69f1c27bb8f4dcb
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2020-05-09 18:28:20 +0300

    Added a test against an alternative, older implementation of the anisotropic rectangular solver

--- tests/test_transverse_deformation.py
+++ tests/test_transverse_deformation.py
@@ -398,4 +398,113 @@ def test_anisotropic_rectangular_vs_isotropic_rectangular():
 
     #Check contact forces
     assert np.all(np.logical_or(np.abs(P_iso(X,Y) - P_aniso(X,Y)) < meps,
-                                np.logical_and(np.isnan(P_iso(X,Y)), np.isnan(P_aniso(X,Y)))))    
+                                np.logical_and(np.isnan(P_iso(X,Y)), np.isnan(P_aniso(X,Y))))) 
+
+
+
+def test_anisotropic_rectangular_vs_old_Version():
+
+    #For compliance matrix
+    ttx = TTcrystal(crystal='Si',hkl=[9,5,3], thickness=Quantity(0.1,'mm'))
+
+    a = 100.0
+    b = 100.0
+
+    x=np.linspace(-a/2,a/2,150)
+    X,Y=np.meshgrid(x,x)
+
+    thickness = 0.1
+
+    Rx = 1000.0
+    Ry = 500.0
+
+    R = np.sqrt(Rx*Ry)
+
+    S = ttx.S.in_units('GPa^-1')    
+
+    stress = {}
+    strain = {}
+    
+    #Numerical solution through solving a linear system A*x = b
+    B = np.array([0,0,0,0,0,0,0,0,-0.5])
+    #Construction of A matrix rows
+    #[C11, C20, C02, C22, C31, C13, C40, C04, lambda]
+    A = []
+    
+    A.append([12*S[5,5], -12*S[1,5], -12*S[0,5], -(S[0,5]*a**2 + S[1,5]*b**2), S[5,5]*a**2, S[5,5]*b**2, -S[1,5]*a**2, -S[0,5]*b**2, 0]) #dL/C11
+    A.append([-12*S[0,5], 12*S[0,1], 12*S[0,0], (S[0,0]*a**2 + S[0,1]*b**2), -S[0,5]*a**2, -S[0,5]*b**2, S[0,1]*a**2, S[0,0]*b**2, 0])   #dL/C02
+    A.append([-12*S[1,5], 12*S[1,1], 12*S[1,0], (S[1,0]*a**2 + S[1,1]*b**2), -S[1,5]*a**2, -S[1,5]*b**2, S[1,1]*a**2, S[0,1]*b**2, 0])   #dL/C20
+    A.append([-(S[0,5]*a**2 + S[1,5]*b**2), S[0,1]*a**2 + S[1,1]*b**2, S[0,0]*a**2 + S[0,1]*b**2, (S[0,1]+2*S[5,5])*a**2*b**2/6 + 3*(S[0,0]*a**4 + S[1,1]*b**4)/20,
+              -(3*S[0,5]*a**4/20 + 5*S[1,5]*a**2*b**2/12), -(3*S[1,5]*b**4/20 + 5*S[0,5]*a**2*b**2/12), 3*S[0,1]*a**4/20 + S[1,1]*a**2*b**2/12, 3*S[0,1]*b**4/20 + S[0,0]*a**2*b**2/12, 2*S[0,1] + S[5,5]]) #dL/C22
+    A.append([S[5,5]*a**2, -S[1,5]*a**2, -S[0,5]*a**2, -(3*S[0,5]*a**4/20 + 5*S[1,5]*a**2*b**2/12), S[1,1]*a**2*b**2/3 + 3*S[5,5]*a**4/20, (S[0,1]/3 + S[5,5]/12)*a**2*b**2, -3*S[1,5]*a**4/20, - S[0,5]*a**2*b**2/12, -2*S[1,5]]) #dL/C31
+    A.append([S[5,5]*b**2, -S[1,5]*b**2, -S[0,5]*b**2, -(3*S[1,5]*b**4/20 + 5*S[0,5]*a**2*b**2/12), (S[0,1]/3 + S[5,5]/12)*a**2*b**2, S[0,0]*a**2*b**2/3 + 3*S[5,5]*b**4/20, -S[1,5]*a**2*b**2/12, -3*S[0,5]*b**4/20, -2*S[0,5]]) #dL/C13
+    A.append([-S[1,5]*a**2, S[1,1]*a**2, S[0,1]*a**2, S[1,1]*a**2*b**2/12 + 3*S[0,1]*a**4/20, -3*S[1,5]*a**4/20, -S[1,5]*a**2*b**2/12, 3*S[1,1]*a**4/20, S[0,1]*a**2*b**2/12, S[1,1]]) #dL/C40
+    A.append([-S[0,5]*b**2, S[0,1]*b**2, S[0,0]*b**2, S[0,0]*a**2*b**2/12 + 3*S[0,1]*b**4/20, -S[0,5]*a**2*b**2/12, -3*S[0,5]*b**4/20, S[0,1]*a**2*b**2/12, 3*S[0,0]*b**4/20, S[0,0]]) #dL/C04
+    A.append([0, 0, 0, 2*S[0,1] + S[5,5], -2*S[1,5], -2*S[0,5], S[1,1], S[0,0], 0])
+
+    A = np.array(A)
+
+    C11, C20, C02, C22, C31, C13, C40, C04, L = np.linalg.solve(A,B)
+
+    stress['xx'] = (C02 + C22*X**2 + 2*C13*X*Y + C04*Y**2)/R**2
+    stress['yy'] = (C20 + C22*Y**2 + 2*C31*X*Y + C40*X**2)/R**2
+    stress['xy'] = -(C11 + 2*C22*X*Y + C31*X**2 + C13*Y**2)/R**2
+    stress['yx'] = stress['xy'].copy()
+
+    strain['zz'] = S[2,0]*stress['xx'] + S[2,1]*stress['yy'] + S[2,5]*stress['xy']
+    strain['xz'] = 0.5*(S[3,0]*stress['xx'] + S[3,1]*stress['yy'] + S[3,5]*stress['xy'] )
+    strain['zx'] = strain['xz'].copy()
+
+
+    #Apply mask
+    mask = np.ones(X.shape)
+    mask[np.abs(X)>a/2] = 0
+    mask[np.abs(Y)>b/2] = 0
+
+    for key in stress:
+        stress[key][mask < 0.5] = np.nan 
+    for key in strain:
+        strain[key][mask < 0.5] = np.nan 
+
+    stress_imp, strain_imp, P_imp = anisotropic_rectangular(Rx, Ry, a, b, thickness, S)
+
+    #add int indexing
+    int2char_ind = ['','x','y','z']
+
+    for i in range(1,3):
+        for j in range(1,3):
+            stress[i*10+j] = stress[int2char_ind[i]+int2char_ind[j]]
+
+    for i in [1,3]:
+        for j in [1,3]:
+            if i==1 and j == 1:
+                continue
+
+            strain[i*10+j] = strain[int2char_ind[i]+int2char_ind[j]]
+    
+    meps = np.finfo(np.float).eps #machine epsilon
+    
+    for i in range(1,3):
+        for j in range(1,3):
+            
+            num_ind = i*10+j
+            str_ind = int2char_ind[i]+int2char_ind[j]
+            
+            assert np.all(np.logical_or(np.abs(stress[num_ind] - stress_imp[num_ind](X,Y)) < meps,
+                                        np.logical_and(np.isnan(stress[num_ind]), np.isnan(stress_imp[num_ind](X,Y)))))
+            assert np.all(np.logical_or(np.abs(stress[str_ind] - stress_imp[str_ind](X,Y)) < meps,
+                                        np.logical_and(np.isnan(stress[str_ind]), np.isnan(stress_imp[str_ind](X,Y)))))
+
+    for i in [1,3]:
+        for j in [1,3]:
+            if i==1 and j == 1:
+                continue
+
+            num_ind = i*10+j
+            str_ind = int2char_ind[i]+int2char_ind[j]
+            
+            assert np.all(np.logical_or(np.abs(strain[num_ind] - strain_imp[num_ind](X,Y)) < meps,
+                                        np.logical_and(np.isnan(strain[num_ind]), np.isnan(strain_imp[num_ind](X,Y)))))
+            assert np.all(np.logical_or(np.abs(strain[str_ind] - strain_imp[str_ind](X,Y)) < meps,
+                                        np.logical_and(np.isnan(strain[str_ind]), np.isnan(strain_imp[str_ind](X,Y)))))
+
