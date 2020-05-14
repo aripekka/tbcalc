86afa1de0e33cda120b6d834400e5ee7b3d89431
Ari-Pekka Honkanen <honkanen.ap@gmail.com>
2020-05-14 23:07:34 +0300

    Added TTscan and TTcrystal to imports for convenience

--- tbcalc/__init__.py
+++ tbcalc/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .analyser import Analyser
-from pyTTE import Quantity
+from pyTTE import Quantity, TTscan, TTcrystal
 from .tensor_transform import cartesian_tensors_to_cylindrical
 
-__all__ = ['Analyser','Quantity','cartesian_tensors_to_cylindrical']
+__all__ = ['Analyser','Quantity','TTscan','TTcrystal','cartesian_tensors_to_cylindrical']
